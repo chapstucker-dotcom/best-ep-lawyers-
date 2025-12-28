c4e3379376f2dc884a99b47085e94738daf2b278
chapstucker-dotcom <chapstucker@gmail.com>
2025-12-27 20:30:16 -0700

    Create AuthCallbackPage for user sign-in redirection

---
+++ auth/callback/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+
+export default function AuthCallbackPage() {
+  useEffect(() => {
+    supabase.auth.getSession().then(() => {
+      window.location.replace("/dashboard");
+    });
+  }, []);
+
+  return <div style={{ padding: 40 }}>Signing you in…</div>;
+}
