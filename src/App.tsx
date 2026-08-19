3ce8ac429dee4884b5b9a03c1c2229cd540be903
Chaps Tucker <chapstucker@gmail.com>
2026-08-18 20:47:48 -0600

    Add working brain injury page

--- src/App.tsx
+++ src/App.tsx
@@ -30,10 +30,10 @@ import WrongfulDeath from "./pages/WrongfulDeath";
 import OvertimePayDisputes from "./pages/OvertimePayDisputes";
 import PracticeAreaPage from "./pages/PracticeAreaPage";
 import WorkersCompensation from "./pages/WorkersCompensation";
+import BrainInjury from "./pages/BrainInjury";
 import Guides from "./pages/Guides";
 import GuideArticle from "./pages/GuideArticle";
 import FeaturedBadge from "./pages/FeaturedBadge";
-
 
 import { practiceAreaPages } from "./data/practiceAreaPages";
 import { AuthProvider } from "./contexts/AuthContext";
@@ -44,45 +44,121 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Index />} />
 
-        {/* Personal Injury keeps its custom page design. */}
         <Route
           path="/el-paso-personal-injury-lawyers"
           element={<PersonalInjury />}
         />
 
         <Route path="/el-paso-wage-hour-lawyers" element={<WageHour />} />
-        <Route path="/el-paso-workplace-discrimination-lawyers" element={<WorkplaceDiscrimination />} />
 
-        <Route path="/el-paso-criminal-defense-lawyers" element={<CriminalDefense />} />
-        <Route path="/el-paso-immigration-lawyers" element={<Immigration />} />
-        <Route path="/el-paso-car-accident-lawyers" element={<CarAccidentLawyers />} />
-        <Route path="/el-paso-dog-bite-lawyers" element={<DogBite />} />
-        <Route path="/el-paso-slip-and-fall-lawyers" element={<SlipAndFall />} />
-        <Route path="/el-paso-truck-accident-lawyers" element={<TruckAccidentLawyers />} />
-        <Route path="/el-paso-motorcycle-accident-lawyers" element={<MotorcycleAccident />} />
-        <Route path="/el-paso-bicycle-accident-lawyers" element={<BicycleAccident />} />
-        <Route path="/el-paso-wrongful-termination-lawyers" element={<WrongfulTermination />} />
-        <Route path="/el-paso-employment-lawyers" element={<EmploymentLaw />} />
-        <Route path="/el-paso-age-discrimination-lawyers" element={<AgeDiscrimination />} />
-        <Route path="/el-paso-sex-discrimination-lawyers" element={<SexDiscrimination />} />
-        <Route path="/el-paso-wrongful-death-lawyers" element={<WrongfulDeath />} />
-        <Route path="/el-paso-overtime-pay-disputes-lawyers" element={<OvertimePayDisputes />} />
         <Route
-  path="/el-paso-workers-compensation-lawyers"
-  element={<WorkersCompensation />}
-/>
+          path="/el-paso-workplace-discrimination-lawyers"
+          element={<WorkplaceDiscrimination />}
+        />
 
-        {/* All other practice-area routes come from one master data file. */}
-        {practiceAreaPages.filter((page) => !["/el-paso-dog-bite-lawyers", "/el-paso-slip-and-fall-lawyers", "/el-paso-truck-accident-lawyers", "/el-paso-bicycle-accident-lawyers", "/el-paso-wrongful-termination-lawyers", "/el-paso-employment-lawyers"].includes(page.path)).map((page) => (
-          <Route
-            key={page.path}
-            path={page.path}
-            element={<PracticeAreaPage />}
-          />
-        ))}
+        <Route
+          path="/el-paso-criminal-defense-lawyers"
+          element={<CriminalDefense />}
+        />
+
+        <Route
+          path="/el-paso-immigration-lawyers"
+          element={<Immigration />}
+        />
+
+        <Route
+          path="/el-paso-car-accident-lawyers"
+          element={<CarAccidentLawyers />}
+        />
+
+        <Route
+          path="/el-paso-dog-bite-lawyers"
+          element={<DogBite />}
+        />
+
+        <Route
+          path="/el-paso-slip-and-fall-lawyers"
+          element={<SlipAndFall />}
+        />
+
+        <Route
+          path="/el-paso-truck-accident-lawyers"
+          element={<TruckAccidentLawyers />}
+        />
+
+        <Route
+          path="/el-paso-motorcycle-accident-lawyers"
+          element={<MotorcycleAccident />}
+        />
+
+        <Route
+          path="/el-paso-bicycle-accident-lawyers"
+          element={<BicycleAccident />}
+        />
+
+        <Route
+          path="/el-paso-wrongful-termination-lawyers"
+          element={<WrongfulTermination />}
+        />
+
+        <Route
+          path="/el-paso-employment-lawyers"
+          element={<EmploymentLaw />}
+        />
+
+        <Route
+          path="/el-paso-age-discrimination-lawyers"
+          element={<AgeDiscrimination />}
+        />
+
+        <Route
+          path="/el-paso-sex-discrimination-lawyers"
+          element={<SexDiscrimination />}
+        />
+
+        <Route
+          path="/el-paso-wrongful-death-lawyers"
+          element={<WrongfulDeath />}
+        />
+
+        <Route
+          path="/el-paso-overtime-pay-disputes-lawyers"
+          element={<OvertimePayDisputes />}
+        />
+
+        <Route
+          path="/el-paso-workers-compensation-lawyers"
+          element={<WorkersCompensation />}
+        />
+
+        <Route
+          path="/el-paso-brain-injury-lawyers"
+          element={<BrainInjury />}
+        />
+
+        {practiceAreaPages
+          .filter(
+            (page) =>
+              ![
+                "/el-paso-dog-bite-lawyers",
+                "/el-paso-slip-and-fall-lawyers",
+                "/el-paso-truck-accident-lawyers",
+                "/el-paso-bicycle-accident-lawyers",
+                "/el-paso-wrongful-termination-lawyers",
+                "/el-paso-employment-lawyers",
+              ].includes(page.path)
+          )
+          .map((page) => (
+            <Route
+              key={page.path}
+              path={page.path}
+              element={<PracticeAreaPage />}
+            />
+          ))}
 
         <Route path="/guides" element={<Guides />} />
         <Route path="/guides/:slug" element={<GuideArticle />} />
+
         <Route
           path="/featured-on-el-pasos-best-lawyers"
           element={<FeaturedBadge />}
@@ -102,18 +178,3 @@ export default function App() {
     </AuthProvider>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
